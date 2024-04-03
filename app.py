2e39f2cdb2da57ffe96555b285fced69fd95cd22
satzbu <51527224+satzbu@users.noreply.github.com>
2024-04-03 21:07:38 +0530

    Add files via upload
    This is my first website

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.set_page_config(page_title="Sathish webpage", page_icon=":tada:",layout="wide")
+with st.container():
+    st.subheader("Hi, I am Sathish :wave:")
+    st.title("A developer from INDIA")
+    st.write("I am a passionate developer from india and originating in tamilnadu.")
